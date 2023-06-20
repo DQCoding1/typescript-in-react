d4c76ddf656e152d13c9b1ef73722a57acb1e955
DQCoding1 <diegoabquintero@gmail.com>
2023-06-19 18:43:45 -0600

    handling props

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Heading from './components/Heading'
+
+function App() {
+  
+  return (
+    <main>
+      <Heading title="Typescript in React"/>
+    </main>
+  )
+}
+
+export default App
---
+++ src/components/Heading.tsx
@@ -0,0 +1,20 @@
+// handle props with destructuring
+const Heading = ({title}: {title:string}) => {
+  return (
+    <header>
+      <p>{title}</p>
+    </header>
+  )
+}
+
+
+// handle props without destructuring
+// const Heading = (props: {title: string}) => {
+//   return (
+//     <header>
+//       <p>{props.title}</p>
+//     </header>
+//   )
+// }
+
+export default Heading
